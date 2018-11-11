a9a2536584097a591f920de638e7b6c8f6928832
afozbek <furkanozbek1995@gmail.com>
2018-11-11 09:57:38 +0300

    :warm:

---
+++ play.js
@@ -0,0 +1,25 @@
+//Async code
+//Callback function which will be done after 
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, 2000);
+  });
+  return promise;
+};
+
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData()
+    .then(text => {
+      console.log(text)
+      return fetchData();
+    })
+    .then(text2 => {
+      console.log(text2);
+    });
+}, 2000);
+//Sync Code
+console.log('Hello!');
+console.log('Hi!');

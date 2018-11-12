2ab78a7924d3486045a4557e168f2e306fe3fdbb
afozbek <afozbek@icloud.com>
2018-11-12 23:05:32 +0300

    Fixing Files

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 const path = require('path');
 
 const express = require('express');
-const body = require('body-parser');
+const body = require('body-parser'); //required middleware for express to get the data from form
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //view engine ejs
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
-const shopRouter = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(body.urlencoded({ extended: false }));
 //Static file location /root/public
 app.use(express.static(path.join(__dirname, 'public')));
 
 //order mathers 
-app.use('/admin', adminData.routes);
-app.use(shopRouter);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: '404' });
-});
+app.use(errorController.get404);
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,12 @@
 const path = require('path');
 const express = require('express');
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (request, response, next) => {
-    //View Model
-    const products = adminData.products;
-    response.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
-});
+router.get('/', productController.getProducts);
+
 module.exports = router;
 
 

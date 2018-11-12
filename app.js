c422dfd13557055fe06f23f48a85e138c135162e
afozbek <furkanozbek1995@gmail.com>
2018-11-12 13:43:02 +0300

    Adding Dynamic Content :lipstick:

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(shopRouter);
 
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,8 @@ const products = [];
 
 // /admin/add-product ==>GET
 router.get('/add-product', (request, response, next) => {
-    response.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // response.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    response.render('add-product', { pageTitle: 'Add Product' });
 });
 
 // /admin/add-product ==>POST

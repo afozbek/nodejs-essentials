adec7c174bd0bb2ad33f29ac33bdf565febecf54
afozbek <furkanozbek1995@gmail.com>
2018-11-25 02:52:33 +0300

    send_mail

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,23 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        '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'
+    }
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
   if (message.length > 0) {
     message = message[0];
-  }
-  else {
+  } else {
     message = null;
   }
   res.render('auth/login', {
@@ -18,11 +28,10 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
-  let message = req.flash('error_sign');
+  let message = req.flash('error');
   if (message.length > 0) {
     message = message[0];
-  }
-  else {
+  } else {
     message = null;
   }
   res.render('auth/signup', {
@@ -70,7 +79,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
-        console.log(req.flash('error_sign', 'Current e-mail is exist!'));
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        );
         return res.redirect('/signup');
       }
       return bcrypt
@@ -85,6 +97,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {

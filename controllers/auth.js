e25915e4b09834c50bd9c4fb1412bd02ca1a9be2
afozbek <furkanozbek1995@gmail.com>
2018-11-24 18:19:37 +0300

    Starting_Authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,18 +8,28 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById('5bf44e46b0d33704dc0072d2')
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save((err) => {
+      req.session.save(err => {
         console.log(err);
         res.redirect('/');
       });
     })
     .catch(err => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {

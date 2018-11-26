b78a5e433c6c13ae8ba5850cce46ad5c432cade7
frkozbk <afozbek@icloud.com>
2018-11-26 22:38:26 +0300

    Resetting_Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -8,7 +10,7 @@ const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
       api_key:
-        //api key
+        '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'
     }
   })
 );
@@ -119,3 +121,52 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'shop@node-complete.com',
+          subject: 'Password reset',
+          html: `
+            <p>You requested a password reset </p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a>
+             to set a new password </p>
+          `
+        });
+      })
+      .catch(err => console.log(err));
+  });
+}

6a1b1493eea53026c0aa819007cbe1936d1d3182
afozbek <furkanozbek1995@gmail.com>
2019-01-07 17:16:21 +0300

    port_ayari

--- app.js
+++ app.js
@@ -107,7 +107,8 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    port = process.env.PORT || 3000
+    app.listen(port);
   })
   .catch(err => {
     console.log(err);
